2ec043ed99200e0d76727d4707b1b8662564a494
CassemiroV <68072791+CassemiroV@users.noreply.github.com>
2025-11-25 23:08:42 -0300

    Add files via upload

---
+++ financeSchedule/src/main/java/kz/schedule/financeSchedule/FinanceScheduleApplication.java
@@ -0,0 +1,13 @@
+package kz.schedule.financeSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceScheduleApplication.class, args);
+	}
+
+}
